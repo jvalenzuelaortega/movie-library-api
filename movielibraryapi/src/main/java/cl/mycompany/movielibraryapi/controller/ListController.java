4b7726838aebb10a9be8801bb86112bad99f8980
Juan Valenzuela <jvalenzuelaortega@gmail.com>
2022-06-10 16:54:18 -0400

    ADD: base controllers are created rest controllers MovieController.java and ListController.java are added

---
+++ movielibraryapi/src/main/java/cl/mycompany/movielibraryapi/controller/ListController.java
@@ -0,0 +1,9 @@
+package cl.mycompany.movielibraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/lists")
+public class ListController {
+}
---
+++ movielibraryapi/src/main/java/cl/mycompany/movielibraryapi/controller/MovieController.java
@@ -0,0 +1,9 @@
+package cl.mycompany.movielibraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/movie")
+public class MovieController {
+}
